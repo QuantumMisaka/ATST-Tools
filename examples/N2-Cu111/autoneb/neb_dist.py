a4f55701f8889178a301020184d265a2ea5b4b33
James Misaka <ff6757442@gmail.com>
2023-11-25 17:21:16 +0800

    update N2-Cu111 example

---
+++ examples/N2-Cu111/autoneb/neb_dist.py
@@ -0,0 +1,23 @@
+# JamesMisaka in 2023-11-16
+# read INIT and FINAL state and give the distance between each atoms
+
+import os, sys
+from ase.io import read, write
+from ase.geometry import distance
+from pathlib import Path
+
+if __name__ == "__main__":
+    msg = '''
+Usage: 
+    python neb_dist.py [init_image] [final_image]
+Will return the distance of two image structure by Frobenius norm
+'''
+    if len(sys.argv) < 3:
+        print(msg)
+    else:
+        init_infile = sys.argv[1]
+        final_infile = sys.argv[2]
+        init_Atoms = read(init_infile)
+        final_Atoms = read(final_infile)
+        distance = distance(init_Atoms, final_Atoms)
+        print(f"==> Distance Between Two Image is {distance:.4f} Angstrom <==")
