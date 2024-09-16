9c0fb92963a6af8ddf03522feb666b660c9a31d9
James Misaka <ff6757442@gmail.com>
2024-09-16 18:42:20 +0800

    dos2unix

--- dimer/dimer_submit.sh
+++ dimer/dimer_submit.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=16
+#SBATCH --ntasks=16
 #SBATCH --cpus-per-task=4
 #SBATCH -J DIMER-ABACUS
 #SBATCH -o running_dimer.out
 #SBATCH -e running_dimer.err
 #SBATCH -p C064M0256G
-#SBATCH --qos=low
+#SBATCH --qos=normal
 
-# JamesMisaka in 2023-11-14
+# JamesMisaka in 2024-09-16
 # workflow of ase-abacus-dimer method
-# for one calculator, ntasks-per-node for mpi, cpus-per-task for openmp
 
 # in developer's PKU-WM2 server
 source /lustre/home/2201110432/apps/miniconda3/etc/profile.d/conda.sh
-conda activate gpaw-intel
-module load abacus/3.7.1-icx
+conda activate ase
+module load abacus/3.7.5-icx
 
 # if one just done neb calculation and done neb_post.py
-python neb2dimer.py neb_latest.traj # or neb.traj
+# python neb2dimer.py neb_latest.traj # or neb.traj
+# python neb2dimer.py STRU_IS STRU_FS
 
 # Job state 
 echo $SLURM_JOB_ID > JobRun.state
