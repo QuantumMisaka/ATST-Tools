d75d130402782587ce113e4cfe5904323e48debb
Sikai Yao <1165524245@qq.com>
2024-11-01 20:10:52 +0800

    update

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="atst-tools",
+    version="0.1",
+    packages=find_packages(),
+    package_dir={"": "."},
+    install_requires=[],
+    python_requires=">=3.9",
+)
